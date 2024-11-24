5fb89081af08e4a89b08a3b2d09944f0918521e5
drinkliquidice <95946444+drinkliquidice@users.noreply.github.com>
2024-11-23 23:14:22 -0500

    I am finally a programmer

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{html,js,astro}"],
+  theme: {
+    fontFamily: {
+      'rubik': ['Rubik']
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
